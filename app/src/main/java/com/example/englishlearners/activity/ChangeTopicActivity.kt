c851b83139bc2aff0d0156e659ae23099bddb71c
limbanga <95570833+limbanga@users.noreply.github.com>
2023-12-03 23:31:31 +0700

    clean code

--- app/src/main/java/com/example/englishlearners/activity/ChangeTopicActivity.kt
+++ app/src/main/java/com/example/englishlearners/activity/ChangeTopicActivity.kt
@@ -1,11 +1,12 @@
-package com.example.englishlearners
+package com.example.englishlearners.activity
 
 import android.os.Bundle
-import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.englishlearners.AppConst
+import com.example.englishlearners.R
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
--- app/src/main/java/com/example/englishlearners/activity/MainActivity.kt
+++ app/src/main/java/com/example/englishlearners/activity/MainActivity.kt
@@ -1,26 +1,18 @@
 package com.example.englishlearners.activity
 
-import android.app.Dialog
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import com.example.englishlearners.AppConst
-import com.example.englishlearners.ChangeTopicActivity
 import com.example.englishlearners.R
+import com.example.englishlearners.dialog.FolderDialog
 import com.example.englishlearners.fragment.HomeFragment
 import com.example.englishlearners.fragment.LibraryFragment
 import com.example.englishlearners.fragment.ProfileFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.bottomsheet.BottomSheetDialog
-import com.google.firebase.database.ktx.database
-import com.google.firebase.ktx.Firebase
 
 
 class MainActivity : AppCompatActivity() {
@@ -38,7 +30,7 @@ class MainActivity : AppCompatActivity() {
                 R.id.menu_item_profile -> replaceFragment(ProfileFragment())
                 R.id.menu_item_library -> replaceFragment(LibraryFragment())
                 R.id.menu_item_add -> {
-                    openDialog()
+                    openBottomDialog()
                     return@setOnItemSelectedListener false
                 }
                 else -> {}
@@ -47,59 +39,21 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun openDialog() {
+    private fun openBottomDialog() {
         val bottomSheetDialog = BottomSheetDialog(this)
-        val bottomSheetView: View = layoutInflater.inflate(R.layout.bottom_dialog_layout, null)
-        bottomSheetDialog.setContentView(bottomSheetView)
+        val view: View = layoutInflater.inflate(R.layout.bottom_dialog_layout, null)
+        bottomSheetDialog.setContentView(view)
 
-        val addFolder: TextView = bottomSheetView.findViewById(R.id.add_folder)
-        val addTopic: TextView = bottomSheetView.findViewById(R.id.add_topic)
+        val addFolder: TextView = view.findViewById(R.id.add_folder)
+        val addTopic: TextView = view.findViewById(R.id.add_topic)
 
         // add folder
         addFolder.setOnClickListener() {
-            // close bottomsheetdialog
+            // close bottomSheetDialog
             bottomSheetDialog.dismiss()
 
-            val customDialog = Dialog(this)
-            val view: View = layoutInflater.inflate(R.layout.add_folder_dialog_layout, null)
-            customDialog.setContentView(view)
-            // set width and height
-            customDialog.window!!.setLayout(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT
-            )
-
-            // get dialog view
-            val cancelButton: Button = view.findViewById(R.id.cancel_button)
-            val okButton: Button = view.findViewById(R.id.ok_btn)
-            val nameEditText: EditText = view.findViewById(R.id.folder_name_edit_text)
-            val descEditText: EditText = view.findViewById(R.id.folder_desc_edit_text)
-
-            cancelButton.setOnClickListener {
-                customDialog.dismiss()
-            }
-
-            okButton.setOnClickListener{
-                val database = Firebase.database
-                //
-                val myRef = database.getReference(AppConst.KEY_FOLDER)
-                val newRef = myRef.push()
-                val data = mapOf(
-                    "name" to nameEditText.text.toString(),
-                    "desc" to descEditText.text.toString(),
-                )
-                newRef.setValue(data)
-                { databaseError, _ ->
-                    if (databaseError != null) {
-                        Toast.makeText(this, "Some thing went wrong", Toast.LENGTH_LONG).show()
-                    } else {
-                        Toast.makeText(this, "Folder created successfully!", Toast.LENGTH_LONG).show()
-                        customDialog.dismiss()
-                    }
-                }
-            }
-
-            customDialog.show()
+            val folderDialog = FolderDialog(this)
+            folderDialog.show()
         }
 
         // add topic
