5240b0df85056e505fd6366e81dcafb36382bb0d
limbanga <95570833+limbanga@users.noreply.github.com>
2023-12-04 07:13:31 +0700

    logout

--- app/src/main/java/com/example/englishlearners/fragment/ProfileFragment.kt
+++ app/src/main/java/com/example/englishlearners/fragment/ProfileFragment.kt
@@ -1,11 +1,16 @@
 package com.example.englishlearners.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.englishlearners.R
+import com.example.englishlearners.activity.LoginActivity
+import com.google.firebase.auth.FirebaseAuth
 
 
 class ProfileFragment : Fragment() {
@@ -15,5 +20,26 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    private lateinit var logoutButton: Button
+    private lateinit var mAuth : FirebaseAuth
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val view = requireView()
+        mAuth = FirebaseAuth.getInstance()
+        // map view
+        logoutButton = view.findViewById(R.id.logout_btn)
+
+        logoutButton.setOnClickListener{
+            signOut()
+        }
+    }
+
+    private fun signOut() {
+        mAuth.signOut()
+        Toast.makeText(requireContext(), "Đăng xuất thành công", Toast.LENGTH_SHORT).show()
+        val intent = Intent(requireContext(), LoginActivity::class.java)
+        startActivity(intent)
     }
 }
