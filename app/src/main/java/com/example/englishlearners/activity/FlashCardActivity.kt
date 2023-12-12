68644f441f03836c7933b1fb3b0d1235927035b8
Trường Trần <tranhuuquangtruong2003@gmail.com>
2023-12-12 22:48:24 +0700

    flash card

--- app/src/main/java/com/example/englishlearners/activity/FlashCardActivity.kt
+++ app/src/main/java/com/example/englishlearners/activity/FlashCardActivity.kt
@@ -1,13 +1,35 @@
 package com.example.englishlearners.activity
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.englishlearners.R
-
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import com.example.englishlearners.model.Vocabulary
+import com.example.englishlearners.FirebaseService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 class FlashCardActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_flash_card)
+        GlobalScope.launch(Dispatchers.Main) {
+            val vocabularies = getVocabularies("-NlK5ffZxq2IG5lk6oo8")
+            showToast("Vocabulary List Length: ${vocabularies.size}")
+        }
+    }
+
+    private suspend fun getVocabularies(topicId: String): List<Vocabulary> {
+        return withContext(Dispatchers.IO) {
+            // Use FirebaseService to get the vocabularies
+            FirebaseService.getVocabularies(topicId)
+        }
+    }
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
