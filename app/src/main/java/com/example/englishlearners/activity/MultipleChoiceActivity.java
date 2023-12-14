b4d03cecceb03065204ed7d600745dd6c53acc13
Trường Trần <tranhuuquangtruong2003@gmail.com>
2023-12-15 00:06:40 +0700

    done flashcard

---
+++ app/src/main/java/com/example/englishlearners/activity/MultipleChoiceActivity.java
@@ -0,0 +1,14 @@
+package com.example.englishlearners.activity;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.englishlearners.R;
+
+public class MultipleChoiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_multiple_choice);
+    }
+}
