b4632f3634e0062ed38932603c8e0b66bc36857f
limbanga <95570833+limbanga@users.noreply.github.com>
2023-12-17 03:41:37 +0700

    done forget pass

--- app/src/main/java/com/example/englishlearners/activity/ForgotPasswordActivity.kt
+++ app/src/main/java/com/example/englishlearners/activity/ForgotPasswordActivity.kt
@@ -1,8 +1,10 @@
 package com.example.englishlearners.activity
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.englishlearners.R
@@ -12,6 +14,7 @@ class ForgotPasswordActivity : AppCompatActivity() {
 
     private lateinit var confirmButton: Button
     private lateinit var emailEditText: EditText
+    private lateinit var loginLink: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,9 +22,14 @@ class ForgotPasswordActivity : AppCompatActivity() {
         // get view
         confirmButton = findViewById(R.id.btn_confirm)
         emailEditText = findViewById(R.id.email_edt)
+        loginLink = findViewById(R.id.login_link)
         // set event
         confirmButton.setOnClickListener {
             forgotPassword()
+        }
+        loginLink.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
         }
     }
 
