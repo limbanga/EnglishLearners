c1282b4950be639048760c9a8e498cc6d3b80375
limbanga <95570833+limbanga@users.noreply.github.com>
2023-12-17 02:34:57 +0700

    add change password

--- app/src/main/java/com/example/englishlearners/activity/ChangePasswordActivity.kt
+++ app/src/main/java/com/example/englishlearners/activity/ChangePasswordActivity.kt
@@ -1,12 +1,21 @@
 package com.example.englishlearners.activity
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.englishlearners.R
 
 class ChangePasswordActivity : AppCompatActivity() {
+
+    private lateinit var backButton: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_change_password)
+        // get view
+        backButton = findViewById(R.id.back_btn)
+        // set event
+        backButton.setOnClickListener {
+            finish()
+        }
     }
 }
