6269e8e528b61381969ee8f5161d4f743da1dd0d
Trường Trần <tranhuuquangtruong2003@gmail.com>
2023-12-16 23:20:04 +0700

    cập nhật typeword

--- app/src/main/java/com/example/englishlearners/activity/TypeWordEnglish.java
+++ app/src/main/java/com/example/englishlearners/activity/TypeWordEnglish.java
@@ -2,10 +2,16 @@ package com.example.englishlearners.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,27 +24,52 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class TypeWordEnglish extends AppCompatActivity {
     TextView tv_quesion;
     EditText edt_answer;
     Button btn_complete;
-    int score = 0;
+    public static int score = 0;
     int currentIndex = 0;
+    TextView tv_score;
     TextView tv_curentState;
+    View feedbackLayout;
+    TextView tv_corect_word;
+    ImageView iv_back;
+    private TextToSpeech textToSpeech;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_type_word_english);
-        getVocabularies("a");
+        getVocabularies("-NlcBn6rw-jpTzwukjj6");
+        back();
+    }
+    private void back(){
+        iv_back = findViewById(R.id.back);
+        iv_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(TypeWordEnglish.this, TypeWordActivity.class);
+                startActivity(intent);
+                score = 0;
+            }
+        });
+
     }
     private boolean Question(Vocabulary word){
         tv_quesion = findViewById(R.id.textViewWord);
         edt_answer = findViewById(R.id.edt_answer);
         tv_quesion.setText(word.getDefinition());
-        if(edt_answer.getText().equals(word.getTerm())){
+        SpeechAutomatical(word.getTerm());
+
+
+        if(edt_answer.getText().toString().equals(word.getTerm())){
+
             return true;
+
+
         }
         else {
             return false;
@@ -47,11 +78,12 @@ public class TypeWordEnglish extends AppCompatActivity {
 
     }
     private void getVocabularies(String topicId) {
+
         DatabaseReference myRef = FirebaseDatabase.getInstance().getReference("vocabularies");
 
         // Query the data based on the "topicId"
-        //myRef.orderByChild("topicId").equalTo(topicId).addListenerForSingleValueEvent(new ValueEventListener() {
-        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+       myRef.orderByChild("topicId").equalTo(topicId).addListenerForSingleValueEvent(new ValueEventListener() {
+        //myRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 ArrayList<Vocabulary> vocabularies = new ArrayList<>();
@@ -67,25 +99,78 @@ public class TypeWordEnglish extends AppCompatActivity {
 
                 }
 
+                if(currentIndex<vocabularies.size()){
+                    tv_quesion = findViewById(R.id.textViewWord);
+                    tv_quesion.setText(vocabularies.get(currentIndex).getDefinition());
+                    btn_complete = findViewById(R.id.CompleteAnswer);
+                    tv_score = findViewById(R.id.score);
+                    edt_answer = findViewById(R.id.edt_answer);
+                    tv_curentState = findViewById(R.id.currentState);
+                    tv_curentState.setText(currentIndex+"/"+vocabularies.size());
 
-                tv_quesion = findViewById(R.id.textViewWord);
-                tv_quesion.setText(vocabularies.get(currentIndex).getDefinition());
-                btn_complete = findViewById(R.id.CompleteAnswer);
-                btn_complete.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        showToast(currentIndex+"");
-                        if(Question(vocabularies.get(currentIndex))){
-                            score ++ ;
-                            showToast("Đúng");
+
+
+                    btn_complete.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+
+                                if(Question(vocabularies.get(currentIndex))){
+                                    feedbackLayout = getLayoutInflater().inflate(R.layout.feedback_correct_type_word, null);
+                                    tv_corect_word = feedbackLayout.findViewById(R.id.tv_correct_word1);
+                                    tv_corect_word.setText(vocabularies.get(currentIndex).getTerm());
+                                    ViewGroup rootView = findViewById(android.R.id.content);
+                                    rootView.addView(feedbackLayout);
+                                    feedbackLayout.setVisibility(View.GONE);
+                                    score ++ ;
+                                    feedbackLayout.setVisibility(View.VISIBLE);
+                                    new Handler().postDelayed(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            feedbackLayout.setVisibility(View.GONE);
+                                            edt_answer.setText("");
+                                            currentIndex ++ ;
+                                            tv_score.setText("Điểm: "+score);
+                                            tv_curentState.setText(currentIndex+"/"+vocabularies.size());
+
+                                            getVocabularies("-NlcBn6rw-jpTzwukjj6");
+                                        }
+                                    }, 2000);
+                                }
+                                else {
+                                    feedbackLayout = getLayoutInflater().inflate(R.layout.feedback_incorrect_type_word, null);
+                                    tv_corect_word = feedbackLayout.findViewById(R.id.tv_correct_word);
+                                    tv_corect_word.setText(vocabularies.get(currentIndex).getTerm());
+                                    ViewGroup rootView = findViewById(android.R.id.content);
+                                    rootView.addView(feedbackLayout);
+                                    feedbackLayout.setVisibility(View.GONE);
+
+                                    feedbackLayout.setVisibility(View.VISIBLE);
+                                    new Handler().postDelayed(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            feedbackLayout.setVisibility(View.GONE);
+                                            edt_answer.setText("");
+                                            currentIndex ++ ;
+                                            tv_score.setText("Điểm: "+score);
+                                            tv_curentState.setText(currentIndex+"/"+vocabularies.size());
+
+                                            getVocabularies("-NlcBn6rw-jpTzwukjj6");
+                                        }
+                                    }, 2000);
+                                }
+
+
+
+
+
                         }
-                        else {
-                            showToast("Sai");
-                        }
-                        currentIndex ++ ;
-                        getVocabularies("abc");
-                    }
-                });
+                    });
+                }
+                else {
+                    Intent intent = new Intent(TypeWordEnglish.this, TypeWordResult.class);
+                    startActivity(intent);
+                }
+
 
 
 
@@ -100,4 +185,42 @@ public class TypeWordEnglish extends AppCompatActivity {
     private void showToast(String message) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
+
+    public void SpeechAutomatical(String word) {
+        textToSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    int langResult = textToSpeech.setLanguage(Locale.US);
+                    if (langResult == TextToSpeech.LANG_MISSING_DATA || langResult == TextToSpeech.LANG_NOT_SUPPORTED) {
+                        //Toast.makeText(FlashCardActivity.this, "Text-to-Speech is not supported on your device.", Toast.LENGTH_SHORT).show();
+                    } else {
+                        speak(word);
+                    }
+                } else {
+
+                }
+            }
+        });
+    }
+
+
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            int langResult = textToSpeech.setLanguage(Locale.US);
+            if (langResult == TextToSpeech.LANG_MISSING_DATA || langResult == TextToSpeech.LANG_NOT_SUPPORTED) {
+                //Toast.makeText(this, "Text-to-Speech is not supported on your device.", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(this, "Initialization failed.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void speak(String text) {
+        if (textToSpeech != null) {
+            textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
+        }
+    }
+
+
 }
---
+++ app/src/main/java/com/example/englishlearners/activity/TypeWordResult.java
@@ -0,0 +1,32 @@
+package com.example.englishlearners.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.englishlearners.R;
+
+public class TypeWordResult extends AppCompatActivity {
+    TextView tv_score;
+    Button btn_exit;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_type_word_result);
+        tv_score = findViewById(R.id.score);
+        tv_score.setText("Điểm: "+TypeWordEnglish.score);
+        btn_exit = findViewById(R.id.btn_exit);
+        btn_exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(TypeWordResult.this, TypeWordActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
